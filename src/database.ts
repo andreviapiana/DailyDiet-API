83f9fc23fb0bd5248611c56bd7eeb8318ac91962
andreviapiana <andreviapiana@gmail.com>
2023-10-27 14:47:39 -0300

    ✨feat: Database - Variáveis de Ambiente e Tratamento do ENV com Zod

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
 import { Knex, knex as setupKnex } from 'knex'
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './database/app.db',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { knex } from './database'
 import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
@@ -21,8 +22,8 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('HTTP Server Running!')
+    console.log(`HTTP Server Running at port ${env.PORT}`)
   })
