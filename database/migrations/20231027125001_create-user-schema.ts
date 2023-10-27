6db78d1ae437f3497cc1607afc23f34ec7bf4739
andreviapiana <andreviapiana@gmail.com>
2023-10-27 13:26:35 -0300

    ✨feat: Database - Criando Tabela de Usuários

--- database/migrations/20231027125001_create-user-schema.ts
+++ database/migrations/20231027125001_create-user-schema.ts
@@ -1,10 +1,16 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('email').notNullable()
+    table.text('address').notNullable()
+    table.decimal('weight', 5, 2).notNullable() // O número 5 indica o total de dígitos e o 2 é o número de casas decimais //
+    table.integer('height').notNullable()
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
 }
-
