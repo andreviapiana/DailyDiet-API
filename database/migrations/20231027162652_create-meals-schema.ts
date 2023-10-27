57889f6c4d59f98b7574d3388f5ff575b4154db2
andreviapiana <andreviapiana@gmail.com>
2023-10-27 13:28:02 -0300

    ✨feat: Database - Criando Tabela de Refeições

---
+++ database/migrations/20231027162652_create-meals-schema.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.uuid('user_id').index()
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.boolean('isOnTheDiet').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
