eee120b0e4bf7460a054428b4db028717f7011dc
andreviapiana <andreviapiana@gmail.com>
2023-10-30 00:01:04 -0300

    ✨feat: Rotas - Listando Apenas Itens do Usuário + Validando o Cookie

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,28 +1,114 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
-import crypto, { randomUUID } from 'node:crypto'
+import crypto from 'node:crypto'
 import { z } from 'zod'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  // Listando todas refeições //
-  app.get('/', async () => {
-    const meals = await knex('meals').select()
+  // Listando todas refeições do Usuário //
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
 
-    return { meals }
-  })
+    async (request) => {
+      const { sessionId } = request.cookies
+
+      const [user] = await knex('users')
+        .where('session_id', sessionId)
+        .select('id')
+
+      const userId = user.id
+
+      // .where('user_id', userId) -> Selecionar apenas onde a coluna user_id seja correspondende ao id do usuário que criou o prato //
+      const meals = await knex('meals').where('user_id', userId).select()
+
+      return { meals }
+    },
+  )
 
   // Listando refeições pelo ID //
-  app.get('/:id', async (request) => {
-    const getMealsParamsSchema = z.object({
-      id: z.string().uuid(),
-    })
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
 
-    const { id } = getMealsParamsSchema.parse(request.params)
+    async (request, response) => {
+      const getMealsParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
 
-    const meal = await knex('meals').where('id', id).first()
+      const { id } = getMealsParamsSchema.parse(request.params)
+      const { sessionId } = request.cookies
 
+      const [user] = await knex('users')
+        .where('session_id', sessionId)
+        .select('id')
+
+      const userId = user.id
+
+      const meal = await knex('meals')
+        .where('id', id)
+        .andWhere('user_id', userId)
+        .first()
+
+      if (!meal) {
+        return response.status(404).send({
+          error: 'Refeição não encontrada',
+        })
+      }
+
+      return {
+        meal,
+      }
+    },
+  )
+
+  app.get('/summary', async (request) => {
+    // Buscando o usuário
+    const { sessionId } = request.cookies
+
+    const [user] = await knex('users')
+      .where('session_id', sessionId)
+      .select('id')
+
+    const userId = user.id
+
+    const [count] = await knex('meals')
+      .count('id', {
+        as: 'Total de refeições registradas',
+      })
+      .where('user_id', userId)
+
+    const refDieta = await knex('meals')
+      .count('id', { as: 'Total de refeições dentro da dieta' })
+      .where('isOnTheDiet', true)
+      .andWhere('user_id', userId)
+
+    const refForaDieta = await knex('meals')
+      .count('id', { as: 'Total de refeições fora da dieta' })
+      .where('isOnTheDiet', false)
+      .andWhere('user_id', userId)
+
+    const summary = {
+      'Total de refeições registradas': parseInt(
+        JSON.parse(JSON.stringify(count))['Total de refeições registradas'],
+      ),
+      'Total de refeições dentro da dieta': parseInt(
+        JSON.parse(JSON.stringify(refDieta))[0][
+          'Total de refeições dentro da dieta'
+        ],
+      ),
+      'Total de refeições fora da dieta': parseInt(
+        JSON.parse(JSON.stringify(refForaDieta))[0][
+          'Total de refeições fora da dieta'
+        ],
+      ),
+    }
     return {
-      meal,
+      summary,
     }
   })
 
